de578611d56969716af13827d7b84bcfc5532f37
KiengPhyl <kiengphilemon@gmail.com>
2022-06-30 14:15:05 -0700

    Write a C program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -> prints exactly after a new line
+ * Return: success always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
