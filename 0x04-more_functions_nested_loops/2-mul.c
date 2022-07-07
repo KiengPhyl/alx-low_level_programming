668f81e844a024219c72d66a1440fb7a30f763dc
KiengPhyl <kiengphilemon@gmail.com>
2022-07-07 14:21:53 -0700

    wow

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> multiplying 2 numbers
+ * @a: interger parameter1
+ * @b: integer parameter 2
+ * Return: success and intiger
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
