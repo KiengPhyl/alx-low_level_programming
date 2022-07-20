34cdc9082b265e6c7de94b8b2411c38fcc999acc
KiengPhyl <kiengphilemon@gmail.com>
2022-07-20 03:43:29 -0700

    trial

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
+
