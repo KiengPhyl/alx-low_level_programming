9b4b4453b69d65c8ae977eec51ec768b9978c91a
KiengPhyl <kiengphilemon@gmail.com>
2022-07-22 11:26:18 -0700

    hytfyyhf

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,24 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", "Error");
-	return (1);
-}
+	int num, j, result = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
-num = atoi(argv[1]);
-if (num < 0)
-{
-	printf("0\n");
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			num -= coins[j];
+			result++;
+		}
+	}
+	printf("%d\n", result);
 	return (0);
 }
-for (j = 0; j < 5 && num >= 0; j++)
-{
-	while (num >= coins[j])
-	{
-		num -= coins[j];
-		result++;
-	}
-}
-printf("%d\n", result);
-return (0);
-}
+	
